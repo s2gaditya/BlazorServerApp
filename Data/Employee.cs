91fb3224f4c7f7c456e5c626d37b55c749baeb22
adityasharma996 <adityasharma996@gmail.com>
2022-07-22 18:06:09 +0530

    Create Employee.cs

---
+++ Data/Employee.cs
@@ -0,0 +1,11 @@
+namespace BlazorServerApp.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+		public string Name { get; set; }
+		
+		public string Address { get; set; }
+    }
+}
