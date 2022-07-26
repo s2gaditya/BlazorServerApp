16417e124cc2324f129ba9d03564680fa2ec5395
adityass2g <109273815+adityass2g@users.noreply.github.com>
2022-07-26 11:59:24 +0530

    Create User.cs

---
+++ Data/User.cs
@@ -0,0 +1,17 @@
+namespace BlazorServerApp.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+		public string FirstName { get; set; }
+		
+		public string LastName { get; set; }
+		
+		public string Address { get; set; }                
+
+        public string? Area { get; set; }
+
+        public string? PinCode { get; set; }
+    }
+}
